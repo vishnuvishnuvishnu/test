227039993d895c8a48b67ecea7d44dda36938958
vishnuvishnuvishnu <vishnusojan@gmail.com>
2017-10-17 00:52:53 +0530

    Add files via upload

---
+++ letter.py
@@ -0,0 +1,6 @@
+name = "vishnu"
+length = len(name)
+print length
+while(length > (-1)):
+	length = length - 1
+	print name[length]
---
+++ lower.py
@@ -0,0 +1,10 @@
+def any_lowercase1(s):
+	for c in s:
+		if c.islower():
+			return True
+		
+		else:
+			return False
+	while(true):
+		print 'yes'
+any_lowercase1('ASDF')
